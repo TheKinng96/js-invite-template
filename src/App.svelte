a6f3d5c073e557c59c9314e789c498f2a9b6ff1b
G3N <fengyuan.yap@gmail.com>
2023-01-15 15:17:37 +0800

    added readme

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,32 @@
-<script lang="ts">
+<!-- <script lang="ts">
 	// Check if token is valid (exist in database + have been reacted)
 </script>
+-->
+<script>
+	let visible = false;
 
-<h1>Pocketbase</h1>
+	function typewriter(node, { speed = 1 }) {
+		const valid = node.childNodes.length === 1 && node.childNodes[0].nodeType === Node.TEXT_NODE;
+
+		if (!valid) {
+			throw new Error(`This transition only works on elements with a single text node child`);
+		}
+
+		const text = node.textContent;
+		const duration = text.length / (speed * 0.01);
+
+		return {
+			duration,
+			tick: (t) => {
+				const i = ~~(text.length * t);
+				node.textContent = text.slice(0, i);
+			}
+		};
+	}
+</script>
+
+<button on:click={() => (visible = !visible)}>Click me</button>
+
+{#if visible}
+	<p in:typewriter={{ speed: 1 }}>The quick brown fox jumps over the lazy dog</p>
+{/if}
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: 'js-invite-template',
+	base: '/js-invite-template',
 	plugins: [svelte()]
 });
